cc3000bd10f74f9e133864c56227ebea7c194cf7
Ricardojsiscol <ricardojslgg@gmail.com>
2025-02-27 21:37:42 -0500

    Created groups for updatable and drawable shapes

--- main.py
+++ main.py
@@ -12,14 +12,21 @@ def main():
     color = (0,0,0)
     clock = pygame.time.Clock()
     dt = 0
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+
+        updatable.update(dt)
         screen.fill(color)
-        player.draw(screen)
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
