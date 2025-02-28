d099f221b37b7a9ca2b7afe437926b815a404f3f
Ricardojsiscol <ricardojslgg@gmail.com>
2025-02-27 19:53:42 -0500

    Added clock and circleshape class

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (0,0,0)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -16,6 +18,7 @@ def main():
 
         screen.fill(color)
         pygame.display.flip()
+        dt += clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
